3def2b367eb0f8ee2c08023472c3f3d59b7988b5
Fleford Redoloza <fleford@gmail.com>
2019-01-13 16:27:01 -0700

    prettified displays

--- Dataset manipulator.py
+++ Dataset manipulator.py
@@ -7,5 +7,18 @@ print(tf.__version__)
 mnist = tf.keras.datasets.mnist
 
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
-print(x_train.dtype)
+print(x_train[0])
 x_train, x_test = x_train / 255.0, x_test / 255.0
+
+# Read in saved model (Be sure to have h5py)
+new_model = tf.keras.models.load_model("myfirstnumreader.model")
+
+variables = tf.trainable_variables()
+print(variables[0])
+# Make a prediction!
+index = 5
+predictions = new_model.predict(x_test)
+print(predictions.shape)
+print(np.argmax(predictions[index]))
+plt.imshow(x_test[index], cmap=plt.cm.binary)
+plt.show()
--- MNIST_NumReader.py
+++ MNIST_NumReader.py
@@ -6,11 +6,14 @@ print(tf.__version__)
 
 mnist = tf.keras.datasets.mnist
 
+class_names = ['0', '1', '2', '3', '4',
+               '5', '6', '7', '8', '9']
+
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 x_train, x_test = x_train / 255.0, x_test / 255.0
 
 model = tf.keras.models.Sequential([
-  tf.keras.layers.Flatten(input_shape=x_train[0].shape),
+  tf.keras.layers.Flatten(input_shape=(28, 28)),
   tf.keras.layers.Dense(512, activation=tf.nn.relu),
   tf.keras.layers.Dropout(0.2),
   tf.keras.layers.Dense(10, activation=tf.nn.softmax)
@@ -19,7 +22,7 @@ model.compile(optimizer='adam',
               loss='sparse_categorical_crossentropy',
               metrics=['accuracy'])
 
-model.fit(x_train, y_train, epochs=3)
+model.fit(x_train, y_train, epochs=1)
 val_loss, val_acc = model.evaluate(x_test, y_test)
 print(val_loss, val_acc)
 
@@ -29,9 +32,49 @@ model.save("myfirstnumreader.model")
 # Read in saved model (Be sure to have h5py)
 new_model = tf.keras.models.load_model("myfirstnumreader.model")
 
+
+# Define handy functions to display predictions
+def plot_image(i, predictions_array, true_label, img):
+    predictions_array, true_label, img = predictions_array[i], true_label[i], img[i]
+    plt.grid(False)
+    plt.xticks([])
+    plt.yticks([])
+
+    plt.imshow(img, cmap=plt.cm.binary)
+
+    predicted_label = np.argmax(predictions_array)
+    if predicted_label == true_label:
+        color = 'blue'
+    else:
+        color = 'red'
+
+    plt.xlabel("{} {:2.0f}% ({})".format(class_names[predicted_label],
+                                         100 * np.max(predictions_array),
+                                         class_names[true_label]),
+               color=color)
+
+
+def plot_value_array(i, predictions_array, true_label):
+    predictions_array, true_label = predictions_array[i], true_label[i]
+    plt.grid(False)
+    plt.xticks([])
+    plt.yticks([])
+    thisplot = plt.bar(range(10), predictions_array, color="#777777")
+    plt.ylim([0, 1])
+    predicted_label = np.argmax(predictions_array)
+
+    thisplot[predicted_label].set_color('red')
+    thisplot[true_label].set_color('blue')
+
+
 # Make a prediction!
-index = 5
-predictions = new_model.predict([x_test])
-print(np.argmax(predictions[index]))
-plt.imshow(x_test[index], cmap=plt.cm.binary)
+predictions = new_model.predict(x_test)
+# i=321 for error
+i = 123
+plt.figure(figsize=(6, 3))
+plt.subplot(1, 2, 1)
+plot_image(i, predictions, y_test, x_test)
+plt.subplot(1, 2, 2)
+plot_value_array(i, predictions,  y_test)
+plt.xticks(range(10), class_names, rotation=45)
 plt.show()
