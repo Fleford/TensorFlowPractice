c8b92859d07dd26e8fe4891a820ac9f4c5fa6855
fleford <fleford@gmail.com>
2019-01-14 14:13:51 -0700

    added mnist reader py (no training)

---
+++ Load_Mnist_Reader.py
@@ -0,0 +1,61 @@
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+
+mnist = tf.keras.datasets.mnist
+
+class_names = ['0', '1', '2', '3', '4',
+               '5', '6', '7', '8', '9']
+
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+x_train, x_test = x_train / 255.0, x_test / 255.0
+
+# Read in saved model (Be sure to have h5py)
+new_model = tf.keras.models.load_model("myfirstnumreader.model")
+
+
+# Define handy functions to display predictions
+def plot_image(i, predictions_array, true_label, img):
+    predictions_array, true_label, img = predictions_array[i], true_label[i], img[i]
+    plt.grid(False)
+    plt.xticks([])
+    plt.yticks([])
+
+    plt.imshow(img, cmap=plt.cm.binary)
+
+    predicted_label = np.argmax(predictions_array)
+    if predicted_label == true_label:
+        color = 'blue'
+    else:
+        color = 'red'
+
+    plt.xlabel("{} {:2.0f}% ({})".format(class_names[predicted_label],
+                                         100 * np.max(predictions_array),
+                                         class_names[true_label]),
+               color=color)
+
+
+def plot_value_array(i, predictions_array, true_label):
+    predictions_array, true_label = predictions_array[i], true_label[i]
+    plt.grid(False)
+    plt.xticks([])
+    plt.yticks([])
+    thisplot = plt.bar(range(10), predictions_array, color="#777777")
+    plt.ylim([0, 1])
+    predicted_label = np.argmax(predictions_array)
+
+    thisplot[predicted_label].set_color('red')
+    thisplot[true_label].set_color('blue')
+
+
+# Make a prediction!
+predictions = new_model.predict(x_test)
+# i=321 for error
+i = 541
+plt.figure(figsize=(6, 3))
+plt.subplot(1, 2, 1)
+plot_image(i, predictions, y_test, x_test)
+plt.subplot(1, 2, 2)
+plot_value_array(i, predictions,  y_test)
+plt.xticks(range(10), class_names, rotation=45)
+plt.show()
