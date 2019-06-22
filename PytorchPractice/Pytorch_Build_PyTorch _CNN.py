c961924d432946c031bda6a7aa7c6cee5da72d42
Fleford Redoloza <fleford@gmail.com>
2019-06-21 20:33:39 -0700

    practicing Pytorch

--- PytorchPractice/Pytorch_Build_PyTorch _CNN.py
+++ PytorchPractice/Pytorch_Build_PyTorch _CNN.py
@@ -1,4 +1,9 @@
+import torch
 import torch.nn as nn
+import torch.nn.functional as F
+
+import torchvision
+import torchvision.transforms as transforms
 
 
 class Network(nn.Module):
@@ -12,8 +17,46 @@ class Network(nn.Module):
         self.out = nn.Linear(in_features=60, out_features=10)
 
     def forward(self, t):
+        # (1) input layer
+        t = t
+
+        # (2) hidden conv layer
+        t = self.conv1(t)
+        t = F.relu(t)
+        t = F.max_pool2d(t, kernel_size=2, stride=2)
+
+        # (3) hidden conv layer
+        t = self.conv2(t)
+        t = F.relu(t)
+        t = F.max_pool2d(t, kernel_size=2, stride=2)
+
+        # (4) hidden linear layer
+        t = t.reshape(-1, 12 * 4 * 4)
+        t = self.fc1(t)
+        t = F.relu(t)
+
+        # (5) hidden linear layer
+        t = self.fc2(t)
+        t = F.relu(t)
+
+        # (6) output layer
+        t = self.out(t)
+        # t = F.softmax(t, dim=1)
+
         return t
 
+
+train_set = torchvision.datasets.FashionMNIST(
+    root='./data/FashionMNIST'
+    , train=True
+    , download=True
+    , transform=transforms.Compose(
+        [transforms.ToTensor()]
+    )
+)
+
+# Turn off gradient calculation
+torch.set_grad_enabled(False)
 
 network = Network()
 print(network.parameters())
