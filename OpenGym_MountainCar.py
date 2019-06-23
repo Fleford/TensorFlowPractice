48e5fa3c9d3fff53a85a1bfb9df1cc963b102595
Fleford Redoloza <fleford@gmail.com>
2019-06-23 01:49:06 -0700

    started gym

--- OpenGym_MountainCar.py
+++ OpenGym_MountainCar.py
@@ -4,23 +4,60 @@ import gym
 env = gym.make("MountainCar-v0")
 env.reset()
 
-print(env.observation_space.high)
-print(env.observation_space.low)
-print(env.action_space.n)
+learning_rate = 0.1
+discount = 0.5
+episodes = 25000
+show_every = 2000
 
 discrete_os_size = [20] * len(env.observation_space.high)
 discrete_os_win_size = (env.observation_space.high - env.observation_space.low) / discrete_os_size
 
-q_table = np.random.uniform(low=-2, high=0)
+epsilon = 0.5
+start_epsilon_decaying = 1
+end_epsilon_decaying = episodes // 2
+
+epsilon_decay_value = epsilon/(end_epsilon_decaying - start_epsilon_decaying)
+
+q_table = np.random.uniform(low=-2, high=0, size=(discrete_os_size + [env.action_space.n]))
 
 
-# done = False
-#
-# while not done:
-#     # 0 = left, 1 = nothing ,2 = right
-#     action = 2
-#     new_state, reward, done, _ = env.step(action)
-#     print(new_state, reward, done)
-#     env.render()
-#
-# env.close()
+def get_discrete_state(state):
+    discrete_state = (state - env.observation_space.low) / discrete_os_win_size
+    return tuple(discrete_state.astype(np.int))
+
+
+for episode in range(episodes):
+    if episode % show_every == 0:
+        print(episode)
+        render = True
+    else:
+        render = False
+
+    discrete_state = get_discrete_state(env.reset())
+    done = False
+    while not done:
+        if np.random.random() > epsilon:
+            # 0 = left, 1 = nothing ,2 = right
+            action = np.argmax(q_table[discrete_state])
+        else:
+            action = np.random.randint(0, env.action_space.n)
+
+        new_state, reward, done, _ = env.step(action)
+        new_discrete_state = get_discrete_state(new_state)
+        if render:
+            env.render()
+        if not done:
+            max_future_q = np.max(q_table[new_discrete_state])
+            current_q = q_table[discrete_state + (action, )]
+            new_q = (1 - learning_rate) * current_q + learning_rate * (reward + discount * max_future_q)
+            q_table[discrete_state + (action, )] = new_q
+        elif new_state[0] >= env.goal_position:
+            print(f"We made it on episode {episode}")
+            q_table[discrete_state + (action, )] = 0
+
+        discrete_state = new_discrete_state
+
+    if end_epsilon_decaying >= episode >= start_epsilon_decaying:
+        epsilon -= epsilon_decay_value
+
+env.close()
